c08b471527b34ea2c605a03089a55ef27aaa3d2b
ferrergustavo <ferrergustavo97@gmail.com>
2019-06-29 14:53:44 -0300

    nuevo db pasado a migrations + otras cosas

---
+++ database/migrations/2019_06_26_203317_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name_p');
+            $table->longtext('description');
+            $table->integer('price');
+            $table->boolean('stock');
+            $table->longtext('img_p');
+            $table->integer('category_id');
+            $table->integer('brand_id');
+            $table->boolean('new');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
