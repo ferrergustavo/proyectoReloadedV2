0b83355c8e326b5710cd6643df887eb4c6b6b027
Enzocons <enzoconstantopoulos@gmail.com>
2019-07-24 02:13:05 -0300

    nav, menu

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,8 +13,12 @@ class SearchController extends Controller
 
     public function search(Request $request){
         $input = $request->input('busqueda');
-        $products = Product::where('name_p','LIKE','%'.$input.'%')->paginate(8);
-        return view('searchResults')->with("products", $products);
+        $products = Product::where('name_p','LIKE','%'.$input.'%')->get();
+        if(count($products)>0){
+            return view('searchResults')->with("products", $products);
+        }else{
+            return view('noResult');
+        }
     }
 
 }
