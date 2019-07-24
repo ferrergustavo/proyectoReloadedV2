293261c5e0bb628d34084f0e919479aa97f4b690
Enzocons <enzoconstantopoulos@gmail.com>
2019-07-23 21:58:24 -0300

    "mas cambios"

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,10 +3,12 @@
 let openProfile = document.querySelector('.profile_logueado');
 let containerProfile = document.querySelector('.profile_display');
 let close_profile = document.querySelector('#imgXI');
+let openProfileLog = document.querySelector('.Perfil__')
 
 
 openProfile.addEventListener('click',showProfile);
 close_profile.addEventListener('click',closeProfile);
+openProfileLog.addEventListener('click',showProfile);
 
 function showProfile(){
     containerProfile.style.display ="block";
